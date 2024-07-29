c0774a460c69ddcf28817bdd17526f18996d2499
JakobHam <72199322+JakobHam@users.noreply.github.com>
2024-07-29 17:01:29 +0200

    day01 version1

--- adventofcode/2023/day01.py
+++ adventofcode/2023/day01.py
@@ -1,11 +1,15 @@
 import os
-
+import re
 
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, 'source.txt')
 
+digit_pattern = re.compile(r'\d')
+
 file = open(file_path, 'r', encoding='utf-8')
 
-print(file)
-
-print('Hello World')
+for line in file:
+    line = line.strip()
+    digits = digit_pattern.findall(line)
+    number = digits[0] + digits[-1]
+    print(number)
