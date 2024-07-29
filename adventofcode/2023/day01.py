e36e50a8bb8c0929a583fdc2ea7c2eaec6cc1b13
JakobHam <72199322+JakobHam@users.noreply.github.com>
2024-07-29 17:06:57 +0200

    day01 part1 final

--- adventofcode/2023/day01.py
+++ adventofcode/2023/day01.py
@@ -6,10 +6,13 @@ file_path = os.path.join(script_dir, 'source.txt')
 
 digit_pattern = re.compile(r'\d')
 
+sum = 0
+
 file = open(file_path, 'r', encoding='utf-8')
 
 for line in file:
     line = line.strip()
     digits = digit_pattern.findall(line)
     number = digits[0] + digits[-1]
-    print(number)
+    sum = sum + int(number)
+print(sum)
